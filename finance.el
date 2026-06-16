1e397b468652b4f3c33d99b8936b820e3a6e08cb
lodenrogue <phyzix5761@gmail.com>
2026-06-17 03:29:51 +0800

    Added macro for doing things to account. Added deleting account

--- finance.el
+++ finance.el
@@ -3,37 +3,51 @@
 (define-derived-mode finance-mode org-mode "Finance Mode"
   "A major mode for tracking finances and budgeting."
   (define-key finance-mode-map (kbd "n") #'finance-add-new-subaccount)
-  (define-key finance-mode-map (kbd "e") #'finance-edit-account))
+  (define-key finance-mode-map (kbd "e") #'finance-edit-account)
+  (define-key finance-mode-map (kbd "d") #'finance-delete-account))
+
+
+(defun finance-delete-account ()
+  "Delete the account at point"
+  (interactive)
+  (finance--do-to-account
+   (delete-line)))
 
 
 (defun finance-edit-account ()
   "Edit the account at point"
   (interactive)
-  (if (org-at-heading-p)
-      (let* ((account-name (org-no-properties (org-get-heading t t t t)))
-	     (new-name (read-string "Account name: " account-name)))
-	(finance--edit-buffer (org-edit-headline new-name)))
-    (message "Point must be on an account to edit it.")))
+  (finance--do-to-account
+   (let* ((account-name (org-no-properties (org-get-heading t t t t)))
+	  (new-name (read-string "Account name: " account-name)))
+     (finance--edit-buffer (org-edit-headline new-name)))))
 
 
 (defun finance-add-new-subaccount ()
   "Create a new child account of the account at point."
   (interactive)
-  (if (org-at-heading-p)
-      (let ((parent-account-name (org-get-heading t t t t))
-	    (parent-level (org-current-level))
-	    (subaccount-name (read-string "New account name: ")))
-	(if (and subaccount-name
-		 (not (string-empty-p subaccount-name)))
-	    (save-excursion
-	      (finance--edit-buffer
-	       (read-only-mode -1)
-	       (end-of-line)
-	       (org-insert-subheading nil)
-	       (insert subaccount-name)
-	       (read-only-mode 1)))
-	  (user-error "Account name cannot be empty.")))
-    (message "Point must be on an account to create a subaccount.")))
+  (finance--do-to-account
+   (let ((parent-account-name (org-get-heading t t t t))
+	 (parent-level (org-current-level))
+	 (subaccount-name (read-string "New account name: ")))
+     (if (and subaccount-name
+	      (not (string-empty-p subaccount-name)))
+	 (save-excursion
+	   (finance--edit-buffer
+	    (read-only-mode -1)
+	    (end-of-line)
+	    (org-insert-subheading nil)
+	    (insert subaccount-name)
+	    (read-only-mode 1)))
+       (user-error "Account name cannot be empty.")))))
+
+
+(defmacro finance--do-to-account (&rest body)
+  "Do the given BODY to an account."
+  `(progn
+     (if (org-at-heading-p)
+	 ,@body
+       (user-error "Point must be on an account to do that."))))
 
 
 (defmacro finance--edit-buffer (&rest body)
